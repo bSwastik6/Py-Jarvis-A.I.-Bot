91340eeca3d3725ec1d85236a1352d216337b0fc
Swastik Banerjee <swastikb06@gmail.com>
2023-01-29 14:09:42 +0530

    Added Features.

---
+++ README.txt
@@ -0,0 +1,9 @@
+Python Jarvis Artificial Intelligence Bot
+
+Features:
+
+1. Speaking function.
+2. Inform current time.
+3. Greet user as per the current time.
+4. Inform current date.
+5. Accepting voice commands.
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from win32com.client import Dispatch
 import datetime
+import pytz
+import speech_recognition as sr
 
 def speak(audio):
     speaking = Dispatch('SAPI.Spvoice')
     speaking.speak(audio)
-# speak("Hello Swastik Sir")
+speak("Hello Swastik Sir")
 
 def time():
     t_now = datetime.datetime.now().strftime('%H:%M:%S')
@@ -15,4 +17,41 @@ def time():
 
 def greet():
     t_hour = datetime.datetime.now().hour
-    print(t_hour)
+    # print(t_hour)
+    if 24 > t_hour <= 4:
+        speak("Have a pleasant night Sir.")
+    elif 12 > t_hour > 4:
+        speak("Good Morning Sir")
+    elif 18 > t_hour >= 12:
+        speak("Good Afternoon Sir")
+    else:
+        speak("Good Evening Sir")
+        speak("Hope you enjoyed your day!")
+
+# greet()
+
+def date():
+    t_date = datetime.datetime.now(tz=pytz.timezone('Asia/Kolkata'))
+    print(t_date.strftime('%d %B, %Y'))
+    speak(t_date.strftime('%d %B, %Y'))
+# date()
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("JARVIS at your service.")
+        print("Command me sir")
+        r.pause_threshold = 1
+        command = r.listen(source)
+    try:
+        print("Recognizing your request.....")
+        recognized = r.recognize_google(command, 'en-in')
+        print(recognized)
+    except Exception as e:
+        print(e)
+        statement = "Pardon Sir. Couldn't process your request."
+        print(statement)
+        speak(statement)
+        return None
+    return recognized
+# take_command()
